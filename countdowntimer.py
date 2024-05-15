10f90c2e1b8b05bf86812a30e5c37fa98b7a7f4f
Alosh Pariyar <aloshpariyar.07@gmail.com>
2024-05-15 08:43:36 +0545

    new file

---
+++ countdowntimer.py
@@ -0,0 +1,14 @@
+#importing time module 
+import time 
+# definig a function for timer
+def countdown(t):
+    while t: 
+        mins, secs = divmod(t, 60)
+        timer = "{:02d}:{:02d}".format(mins, secs)
+        print(timer)
+        time.sleep(1)
+        t-=1
+    print("The Timer has ended")
+# Taking input from the user
+t = input("Enter the time in seconds : ")
+countdown(int(t))
