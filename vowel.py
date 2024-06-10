f37bb8a27948fe8f417a7cb5637b7d03a018db6d
Alosh Pariyar <167700917+Shadow-144@users.noreply.github.com>
2024-06-10 01:57:32 +0000

    Delete vowel.py

--- vowel.py
+++
@@ -1,8 +0,0 @@
-#Taking input from the user and printing the vowels in it
-
-str = input("Enter the string : ")
-b = str.lower()
-for i in range(len(str)):
-    a = b[i]
-    if (a == "a") or (a == "e") or (a == "i") or (a == "o") or (a == "u"):
-        print (a)
