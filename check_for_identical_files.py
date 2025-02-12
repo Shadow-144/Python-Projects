98d3fee0779c49426fadf3eb3cfec947aba17791
Alosh Pariyar <aloshpariyar.07@gmail.com>
2025-02-12 17:13:52 +0545

    fixed some issues

--- check_for_identical_files.py
+++ check_for_identical_files.py
@@ -9,24 +9,25 @@ def hash_file(file1, file2):
     h2 = hashlib.sha1()
 
 # read the contents of file_1 in binary mode 
-    with open ("p1.pdf", "rb") as f_1:
-        chuck = 0
-        for i in range(chuck!=b''):
-            chunck = f_1.read(1024)
-            h2.update(chunck)
+    with open (file1, "rb") as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)
+            h1.update(chunk)
 
 # read the contents of file_2 in binary mode
-    with open ("p2.pdf", "rb") as f_1:
-        chuck = 0
-        for i in range(chuck!=b''):
-            chunck = f_1.read(1024)
-            h1.update(chunck)
-
+    with open (file2, "rb") as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)
+            h2.update(chunk)
+# returning the output of the funtion
     return h1.hexdigest(), h2.hexdigest()
 
-msg1, msg2 = hash_file("p1.pdf", "p2.pdf")
+# output of the function
+msg1, msg2 = hash_file("p1.pdf", "p1 copy.pdf")
 
-
+# last condition to check for the output
 if(msg1!= msg2):
     print("The files are not identical")
 
